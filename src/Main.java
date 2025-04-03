8ea98a146720d5164736d7133afecd85aa7454cb
이원형 <circle9@naonsoft.com>
2025-04-03 10:41:17 +0900

    첫 커밋

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("기본 세팅");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
